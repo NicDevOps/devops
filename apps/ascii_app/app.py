a0ed315904a649e18a6858ec2c33f1137262dadc
nick <aeolidiella@gmail.com>
2020-05-11 22:34:14 -0400

    fixed ascii_app

--- apps/ascii_app/app.py
+++ apps/ascii_app/app.py
@@ -5,8 +5,8 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-list_of_files = glob.glob('ascii_files/*.txt')
-latest_file = max(list_of_files, key=os.path.getmtime)
+list_of_files = sorted(glob.glob('ascii_files/*.txt'))
+latest_file = list_of_files[-1]
 f = open(latest_file, 'r')
 
 print(f.read())
