5604bce5ad2d7f7814c2f898593792d54c5d2e64
nick <aeolidiella@gmail.com>
2020-05-17 04:49:14 -0400

    added raspberry pi 'rpi' folder

--- rpi/led.py
+++ rpi/led.py
@@ -1,19 +1,17 @@
-import RP1.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(false)
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 ledPin = 12
 
 GPIO.setup(ledpin, GPIO.OUT)
 
-fori in range(5):
-
-print("LED turning on")
-GPIO.output(ledpin, GPIO.HIGH)
-time.sleep(0.5)
-
-print("LED turning off")
-GPIO.output(ledpin, GPIO.LOW)
-time.sleep(0.5)
+for i in range(5):
+    print("LED turning on")
+    GPIO.output(ledpin, GPIO.HIGH)
+    time.sleep(0.5)
+    print("LED turning off")
+    GPIO.output(ledpin, GPIO.LOW)
+    time.sleep(0.5)
