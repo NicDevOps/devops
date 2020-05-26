06940c1202e6892e995eddba88ae61ff9560b245
nick <aeolidiella@gmail.com>
2020-05-25 23:08:39 -0400

    rpi cicd test

--- apps/rpi/run.sh
+++ apps/rpi/run.sh
@@ -4,6 +4,8 @@ echo 'running rpi app..'
 
 #scp ~/projects/devops/apps/rpi/test123.py pi@rpi:~/projects/rpi
 
-ssh pi@rpi 'GIT_WORK_TREE=/home/pi/projects/devops git checkout -f'
+#ssh pi@rpi 'GIT_WORK_TREE=/home/pi/projects/devops git checkout -f'
+
+ssh pi@rpi 'hostname'
 
 echo 'works!'
