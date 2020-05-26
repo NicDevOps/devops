75bbec6d894ef91d27806449e9023327d31bdae7
nick <aeolidiella@gmail.com>
2020-05-25 22:58:27 -0400

    rpi: added ssh in run rpi

--- apps/rpi/run.sh
+++ apps/rpi/run.sh
@@ -4,5 +4,5 @@ echo 'running rpi app..'
 
 #scp ~/projects/devops/apps/rpi/test123.py pi@rpi:~/projects/rpi
 
-GIT_WORK_TREE=/home/pi/projects/devops git checkout -f
+ssh pi@rpi 'GIT_WORK_TREE=/home/pi/projects/devops git checkout -f'
 
