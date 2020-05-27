17d6a8e3c463e9d5fb025a6fa0995efa56b6c39d
nick <aeolidiella@gmail.com>
2020-05-26 22:46:07 -0400

    test

--- apps/rpi-discord/build.sh
+++ apps/rpi-discord/build.sh
@@ -6,6 +6,6 @@ echo -e "$MAGENTA###############################################################
 
 set -x
 
-ssh pi@rpi 'git -C /home/pi/projects/devops checkout -f'
+# ssh pi@rpi 'git -C /home/pi/projects/devops checkout -f'
 
-ssh pi@rpi 'cd /home/pi/projects/devops/apps/rpi-discord; docker-compose build'
+# ssh pi@rpi 'cd /home/pi/projects/devops/apps/rpi-discord; docker-compose build'
