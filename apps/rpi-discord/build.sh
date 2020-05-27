2c5b910851d5bba7f3131cb2638805712ee12877
nick <aeolidiella@gmail.com>
2020-05-26 22:52:00 -0400

    test

--- apps/rpi-discord/build.sh
+++ apps/rpi-discord/build.sh
@@ -8,6 +8,6 @@ echo -e "$RED###################################################################
 
 set -x
 
-# ssh pi@rpi 'git -C /home/pi/projects/devops checkout -f'
+ssh pi@rpi 'git -C /home/pi/projects/devops checkout -f'
 
-# ssh pi@rpi 'cd /home/pi/projects/devops/apps/rpi-discord; docker-compose build'
+ssh pi@rpi 'cd /home/pi/projects/devops/apps/rpi-discord; docker-compose build'
