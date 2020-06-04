676b942f5575ad5370b6f6c6be3b5c1403c511e8
pi <pi@rpi>
2020-06-04 03:52:21 +0100

    added checks for temperature and humidity

--- apps/rpi-dht11/app.py
+++ apps/rpi-dht11/app.py
@@ -9,15 +9,19 @@ start_http_server(5000)
 while True:
 
     try:
-      humidity, temperature = Adafruit_DHT.read_retry(11, 17)
+        humidity, temperature = Adafruit_DHT.read_retry(11, 17)
     except Exception as e:
         print('!!!! we caught an exception !!!!')
         print(e)
 
-    t.set(temperature)
-    h.set(humidity)
+    if temperature:
+        t.set(temperature)
 
-    print(f'temperature: {temperature}\nhumidity: {humidity}')
+    if humidity:
+        h.set(humidity)
+
+    if temperature and humidity:
+        print(f'temperature: {temperature}\nhumidity: {humidity}')
 
     time.sleep(1)
 
