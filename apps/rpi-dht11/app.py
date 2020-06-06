534c7dda5883e9bc9f67ad68cf10d1e8ec6ae83a
pi <pi@rpi>
2020-06-06 01:59:54 +0100

    rpi-dht11: added illuminance to metrics

--- apps/rpi-dht11/app.py
+++ apps/rpi-dht11/app.py
@@ -1,15 +1,34 @@
 from prometheus_client import start_http_server, Gauge
 import Adafruit_DHT
 import time
+import RPi.GPIO as GPIO, time, os
+
+GPIO.setmode(GPIO.BCM)
 
 t = Gauge('temperature', 'Temperature')
 h = Gauge('humidity', 'Humidity')
+i = Gauge('illuminance', 'Illuminance')
+
+
+def RCtime(RCpin):
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
+
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while GPIO.input(RCpin) == GPIO.LOW:
+        reading += 1
+    return reading
+
 
 start_http_server(5000)
 while True:
 
     try:
         humidity, temperature = Adafruit_DHT.read_retry(11, 17)
+        illuminance = RCtime(22)
     except Exception as e:
         print('!!!! we caught an exception !!!!')
         print(e)
@@ -19,9 +38,11 @@ while True:
 
     if humidity:
         h.set(humidity)
+    
+    i.set(illuminance)
 
     if temperature and humidity:
-        print(f'temperature: {temperature}\nhumidity: {humidity}')
+        print(f'temperature: {temperature}\nhumidity: {humidity}\nilluminance: {illuminance}')
 
     time.sleep(1)
 
