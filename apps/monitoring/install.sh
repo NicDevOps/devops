1bcfd8cf6943a7f76e82f64460cc4199c318ba53
nick <nick@ur>
2020-06-10 00:45:46 -0400

    fix prometheus grafana nodeexporter install script

--- apps/monitoring/install.sh
+++ apps/monitoring/install.sh
@@ -2,8 +2,20 @@
 
 set -x
 
-sudo cp ./files/nodeexporter.service /etc/systemd/system/nodeexporter.service
+ARCH=$(uname -m)
 
+if [ $ARCH = "x86_64" ];
+then
+  curl -SL https://github.com/prometheus/node_exporter/releases/download/v1.0.0/node_exporter-1.0.0.linux-amd64.tar.gz > node_exporter.tar.gz
+else
+  curl -SL https://github.com/prometheus/node_exporter/releases/download/v1.0.0/node_exporter-1.0.0.linux-armv6.tar.gz > node_exporter.tar.gz
+fi
+
+sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
+
+sudo chmod +x /usr/local/bin/node_exporter
+
+sudo cp ./files/nodeexporter.service /etc/systemd/system/nodeexporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable nodeexporter
