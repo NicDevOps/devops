a137539c0be8f9c61a49073862f1c66677e07c82
nick <aeolidiella@gmail.com>
2020-06-13 01:26:34 -0400

    rpi-sensors: fixed run.sh

---
+++ apps/rpi-sensors/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/projects/devops/cicd/functions.sh
+
+echo -e "$RED######################################################################################$END"
+echo -e "$RED#                                  RUN: RPI-SENSORS                                  #$END"
+echo -e "$RED######################################################################################$END"
+
+set -x
+
+# docker-compose up -d
