6158423762c490f7469bb0cdde3ce170b82fd85e
Nick <aeolidiella@gmail.com>
2020-11-09 22:26:53 -0500

    gitignore

--- cs50/agreed.c
+++ cs50/agreed.c
@@ -9,10 +9,10 @@ int main(void)
     char c = get_char("Do you agree?\n");
 
     // Check whether agreed
-    if (c == 'Y' || c == 'y') 
+    if (c == 'Y' || c == 'y')  
     {
         printf("Agreed.\n");
-    }
+    } 
     else if (c == 'N' || c == 'n')
     {
         printf("Not agreed.\n");
