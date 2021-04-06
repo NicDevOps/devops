c4f375fe36eb065def43b4a1201c7eb954a7e791
Nick <aeolidiella@gmail.com>
2021-04-06 14:02:44 -0400

    cs50 week 7, SQL

---
+++ sqlite3/tutorial.py
@@ -0,0 +1,49 @@
+# sqlite3 python3 tutorial 
+
+import sqlite3
+from contextlib import closing
+
+connection = sqlite3.connect("aquarium.db")
+
+print(connection.total_changes)
+
+cursor = connection.cursor()
+# cursor.execute("CREATE TABLE fish (name TEXT, species TEXT, tank_number INTEGER)")
+
+cursor.execute("INSERT INTO fish VALUES ('Sammy', 'shark', 1)")
+cursor.execute("INSERT INTO fish VALUES ('Jamie', 'cuttlefish', 7)")
+
+rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
+print(rows)
+
+target_fish_name = "Jamie"
+rows = cursor.execute("SELECT name, species, tank_number FROM fish WHERE name = ?",
+(target_fish_name,),
+).fetchall()
+print(rows)
+
+new_tank_number = 2
+moved_fish_name = "Sammy"
+cursor.execute(
+    "UPDATE fish SET tank_number = ? WHERE name = ?",
+    (new_tank_number, moved_fish_name)
+)
+
+rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
+print(rows)
+
+# release sammy into the wild ;)
+
+released_fish_name = "Sammy"
+cursor.execute(
+    "DELETE FROM fish WHERE name = ?",
+    (released_fish_name,)
+)
+
+rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
+print(rows)
+
+with closing(sqlite3.connect("aquarium.db")) as connection:
+    with closing(connection.cursor()) as cursor:
+        rows = cursor.execute("SELECT 1").fetchall()
+        print(rows)
