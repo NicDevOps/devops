da09987bc4f52a457d450e3cc295a740b8bed7dd
Nick <aeolidiella@gmail.com>
2021-10-03 13:14:31 -0400

    cs 50 week 7 movies.py

--- cs50_python/movies/movies.py
+++ cs50_python/movies/movies.py
@@ -4,53 +4,113 @@ connection = sqlite3.connect("movies.db")
 
 cursor = connection.cursor()
 
-# show_tables = cursor.execute(".schema").fetchall()
+# query_limit = 3
 
-# print(show_tables)
+# sql_query = '''
+# select *
+# from movies
+# limit %s;
+# ''' % (query_limit)
+
+# rows = cursor.execute(sql_query).fetchall()
+# print(rows)
 
 # 1
-# rows = cursor.execute("SELECT title FROM movies WHERE year = '2008'").fetchall()
+# sql_query = '''
+# SELECT title
+# FROM movies
+# WHERE year = '2008';
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 2
-# rows = cursor.execute("SELECT birth FROM people WHERE name = 'Emma Stone'").fetchall()
+# sql_query = '''
+# SELECT birth
+# FROM people 
+# WHERE name = 'Emma Stone';
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 3
-# rows = cursor.execute("SELECT title FROM movies WHERE year > 2018 OR year = 2018 ORDER BY year ASC").fetchall()
+# sql_query = '''
+# SELECT title 
+# FROM movies 
+# WHERE year > 2018 OR year = 2018 
+# ORDER BY year ASC;
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 4
-# rows = cursor.execute("SELECT COUNT(rating) FROM ratings where rating = 10.0").fetchall()
+# sql_query = '''
+# SELECT COUNT(rating)
+# FROM ratings 
+# WHERE rating = 10.0;
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 5
-# rows = cursor.execute("SELECT title, year FROM movies WHERE title LIKE '%Harry Potter%' ORDER BY year ASC").fetchall()
+# sql_query = '''
+# SELECT title, year 
+# FROM movies 
+# WHERE title 
+# LIKE '%Harry Potter%' 
+# ORDER BY year ASC;
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 6
-# rows = cursor.execute("SELECT AVG(rating) FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012)").fetchall()
+# sql_query = '''
+# SELECT AVG(rating) 
+# FROM ratings 
+# WHERE movie_id 
+# IN (SELECT id FROM movies WHERE year = 2012);
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 7
-# rows = cursor.execute("SELECT movie_id, rating FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2010) ORDER BY rating DESC").fetchall()
-# print(rows)
-
-# 7
-# rows = cursor.execute("SELECT movies.title, ratings.rating FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC, title ASC").fetchall()
+# sql_query = '''
+# SELECT movies.title, ratings.rating 
+# FROM movies 
+# INNER JOIN ratings 
+# ON movies.id = ratings.movie_id 
+# ORDER BY rating DESC, title ASC;
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
 # 8
-# rows = cursor.execute("SELECT people.name FROM ((people INNER JOIN stars ON people.id = stars.person_id) INNER JOIN movies ON stars.movie_id = movies.id) WHERE title = 'Toy Story' UNION SELECT people.name FROM ((people INNER JOIN directors ON people.id = directors.person_id) INNER JOIN movies ON directors.movie_id = movies.id) WHERE title = 'Toy Story'").fetchall()
+# sql_query = '''
+# SELECT people.name 
+# FROM ((people INNER JOIN stars ON people.id = stars.person_id) 
+# INNER JOIN movies ON stars.movie_id = movies.id) 
+# WHERE title = 'Toy Story' 
+# UNION 
+# SELECT people.name 
+# FROM ((people INNER JOIN directors ON people.id = directors.person_id) 
+# INNER JOIN movies ON directors.movie_id = movies.id) 
+# WHERE title = 'Toy Story';
+# '''
+# rows = cursor.execute(sql_query).fetchall()
 # print(rows)
 
-query_limit = 3
+# 9
+# sql_query = '''
+# SELECT DISTINCT people.name
+# FROM ((people INNER JOIN stars ON people.id = stars.person_id)
+# INNER JOIN movies ON stars.movie_id = movies.id)
+# WHERE year = '2004'
+# UNION
+# SELECT DISTINCT people.name
+# FROM ((people INNER JOIN directors ON people.id = directors.person_id)
+# INNER JOIN movies ON directors.movie_id = movies.id) 
+# WHERE year = '2004';
+# '''
+# rows = cursor.execute(sql_query).fetchall()
+# print(rows)
 
-sql_query = '''
-select *
-from movies
-limit %s;
-''' % (query_limit)
-
-rows = cursor.execute(sql_query).fetchall()
-print(rows)
